cad6f579ff359604bd6a962f0f6aa69b12dd02fb
Arief Badal <1042170@hr.nl>
2022-06-21 20:40:57 +0200

    Add Flask socketio version of the server

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,23 @@
+from flask_socketio import SocketIO
 from flask import Flask, render_template
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
+@app.route('/chat')
+def chat():
+    return render_template('chat.html')
+
+@socketio.on('connect')
+def test_connect():
+    socketio.emit('my response', {'data': 'Connected'})
+
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
